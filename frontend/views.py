59ae2fdbc7fe4d18dc950e2077e6fad1b36c503b
LeeHanByul <byul9123@gmail.com>
2024-06-12 14:44:49 +0900

    front

--- frontend/views.py
+++ frontend/views.py
@@ -7,12 +7,9 @@ import logging
 from django.http import JsonResponse
 from rest_framework_simplejwt.tokens import RefreshToken
 from django.shortcuts import render
-from rest_framework.response import Response
-from articles.models import Article
-from articles.serializers import ArticleSerializer
-
+from django.http import JsonResponse, HttpResponseRedirect
+from django.urls import reverse
 logger = logging.getLogger(__name__)
-
 def login(request):
     if request.method == 'POST':
         form = AuthenticationForm(request, data=request.POST)
@@ -23,12 +20,15 @@ def login(request):
             if user is not None:
                 auth_login(request, user)
                 refresh = RefreshToken.for_user(user)
-                response = JsonResponse({'detail': '로그인 성공', 'refresh': str(refresh), 'access': str(refresh.access_token)})
+                response = JsonResponse({
+                    'detail': '로그인 성공',
+                    'refresh': str(refresh),
+                    'access': str(refresh.access_token)
+                })
                 response.set_cookie('access_token', str(refresh.access_token), httponly=True)
                 response.set_cookie('refresh_token', str(refresh), httponly=True)
-                next_url = request.POST.get('next', 'articles:Camping_list')
-                response['Location'] = next_url
-                return response
+                # 로그인 성공 시 특정 사용자의 상세 페이지로 리디렉션
+                return HttpResponseRedirect(reverse('frontend:user_detail', args=[username]))
             else:
                 logger.debug(f'User {username} authentication failed')
                 return render(request, 'frontend/login.html', {'form': form, 'error': 'Invalid login details'})
@@ -38,22 +38,17 @@ def login(request):
     else:
         form = AuthenticationForm()
     return render(request, 'frontend/login.html', {'form': form})
-
 def signup(request):
     return render(request, 'frontend/signup.html')
-
 def user_detail(request, username):
     user = get_object_or_404(User, username=username)
     return render(request, 'frontend/user_detail.html', {'user': user})
-
 def logout(request):
     auth_logout(request)
-    return redirect('articles:Camping_list')
-
+    return redirect('frontend:index')
 class UserEditView(APIView):
     def get(self, request, username):
         user = get_object_or_404(User, username=username)
         return render(request, 'frontend/useredit.html', {'user': user})
-
 def index(request):
     return render(request, 'frontend/index.html')
